3e4752c7b63e4bb03bfc7db9a6b36d96ac692efd
SandeshJadhav7320 <sandeshj9995@gmail.com>
2025-08-07 10:40:13 +0530

    Search Insert Position

---
+++ Day 11/SearchInsertPosition.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+class SearchInsertPosition 
+{
+    public int searchInsert(int[] nums, int target) 
+    {
+        int start=0;
+        int end=nums.length-1;
+        while(start<=end)
+        {
+            int mid=start+(end-start)/2;
+            if(nums[mid]==target)
+            {
+                return mid;
+            }
+            if(nums[mid]<target)
+            {
+                start=mid+1;
+            }
+            else
+            {
+                end=mid-1;
+            }
+
+
+        }
+        return start;
+    }
+
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the size of the array :- ");
+        int size=sc.nextInt();
+
+        int arr[]=new int[size];
+       System.out.println("Enter the elements in the array :- ");
+        for(int i=0;i<arr.length;i++)
+        {
+            int ele=sc.nextInt();
+            arr[i]=ele;
+        }
+        System.out.println("Enter the number you want to search :- ");
+        int target=sc.nextInt();
+        SearchInsertPosition sn=new SearchInsertPosition();
+        int res=sn.searchInsert(arr,target);
+        System.out.println(res);
+    }
+}
