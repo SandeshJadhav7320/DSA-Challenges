53c5a4bd181e7591ec58d4bd3fb0d4e297baefc8
SandeshJadhav7320 <sandeshj9995@gmail.com>
2025-08-14 23:12:36 +0530

    Return Avergae Value

--- Day 16/SlidingWindow.java
+++ Day 16/SlidingWindow.java
@@ -1,42 +1,45 @@
 import java.util.Scanner;
-public class SlidingWindow 
-{
-    public int Demo(int arr[],int w,int size)
-    {
-        int current=0;
-        for(int i=0;i<w;i++)
-        {
-            current+=arr[i];
 
+public class SlidingWindow {
+    public double findMaxAverage(int[] nums, int k) {
+        double currentSum = 0;
+
+        // First window sum
+        for (int i = 0; i < k; i++) {
+            currentSum += nums[i];
         }
-        int max=current;
-        for(int i=1;i<=size-w;i++)
-        {
-            current=current-arr[i-1]+arr[i+w-1];
-            if(current>max)
-            {
-                max=current;
+
+        double maxSum = currentSum;
+
+        // Slide the window
+        for (int i = k; i < nums.length; i++) {
+            currentSum += nums[i] - nums[i - k];
+            if (currentSum > maxSum) {
+                maxSum = currentSum;
             }
         }
-        return max;
-    }
-    public static void main(String args[])
-    {
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Enter the size if the array :- ");
-        int size=sc.nextInt();
 
-        int arr[]=new int[size];
+        return maxSum / k;
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the size of the array :- ");
+        int size = sc.nextInt();
+
+        int arr[] = new int[size];
         System.out.println("Enter the elements :- ");
-        for(int i=0;i<arr.length;i++)
-        {
-            int ele=sc.nextInt();
-            arr[i]=ele;
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
         }
-        System.out.println("Enter the size of sub array :- ");
-        int w=sc.nextInt();
-        SlidingWindow sl=new SlidingWindow();
-        int max=sl.Demo(arr,w,size);
-        System.out.println("Maximmum sum is :-"+max);
+
+        System.out.println("Enter the size of subarray :- ");
+        int k = sc.nextInt();
+
+        SlidingWindow obj = new SlidingWindow();
+        double maxAvg = obj.findMaxAverage(arr, k);
+
+        System.out.printf("Maximum average is: %.5f\n", maxAvg);
     }
 }
