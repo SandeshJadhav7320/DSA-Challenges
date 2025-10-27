8eba6b56a387ab40df03effdcbbf846677ca637c
SandeshJadhav7320 <sandeshj9995@gmail.com>
2025-10-27 10:52:30 +0530

    Stack using collection framework

---
+++ Day 34/StackUsingCollectionFramework.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class StackUsingCollectionFramework 
+{
+    public static void main(String[] args) 
+    {
+        Stack<Integer> s=new Stack<>();
+        s.push(10);
+        s.push(20);
+        s.push(30);
+        s.push(40);
+        s.push(50);
+
+        while(!s.isEmpty())
+        {
+            System.out.println(s.peek());
+            s.pop();
+        }
+
+    }
+}
