33a366d7fa12b744a373593818f08b522e37e95a
SandeshJadhav7320 <sandeshj9995@gmail.com>
2025-10-29 22:31:50 +0530

    Queue Using Collection Framework

---
+++ Day 37/QueueUsingCollectionFramework.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class QueueUsingCollectionFramework 
+{
+    public static void main(String[] args) 
+    {
+        // Queue<Integer> q = new LinkedList<>();    Using Linked list
+        Queue<Integer> q = new ArrayDeque<>();       //  Using array
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.add(4);
+
+        while(!q.isEmpty())
+        {
+            System.out.println(q.peek());
+            q.remove();
+        }
+    }
+}
